901967a69dcbb03f9f5bd547c5122de24cf6c95c
huangzhilong <964123660@qq.com>
2016-04-07 14:11:27 +0800

    topicOnline test

--- push-server/test/topicOnline.js
+++ push-server/test/topicOnline.js
@@ -2,7 +2,7 @@ var config = require('../config.js');
 
 var redis = require('redis').createClient();
 var io = require('socket.io');
-var topicOnline = require('../lib/stats/topicOnline.js')(redis, io, 'Ys7Gh2NwDY9Dqti92ZwxJh8ymQL4mmZ2 ', config.topicOnlineFilter);
+var topicOnline = require('../lib/stats/topicOnline.js')(redis, io, 'Ys7Gh2NwDY9Dqti92ZwxJh8ymQL4mmZ2 ', ['topic:', 'message']);
 
 var chai = require('chai');
 var expect = chai.expect;
@@ -19,7 +19,10 @@ describe('api topicOnline', function () {
                 expect(result).to.be.equal(2);
                 topicOnline.getTopicOnline('xxxx', function(result){
                     expect(result).to.be.equal(0);
-                    done();
+                    topicOnline.getTopicOnline('testTopic2', function(result){
+                        expect(result).to.be.equal(0);
+                        done();
+                    });
                 });
             });
         }, 1000);
