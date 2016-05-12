d90dfb9ba720ea8afd26a7cdbb05875828b68569
许铎 <xuduo@yy.com>
2016-05-12 18:34:39 +0800

    ---

--- push-server/lib/service/packetService.js
+++ push-server/lib/service/packetService.js
@@ -10,56 +10,50 @@ function PacketService(redis, subClient) {
 }
 
 PacketService.prototype.publishPacket = function (data) {
-    if (this.redis) {
-        var path = data.path;
-        var pushId = data.pushId;
-        if (path && pushId) {
-            if (!data.sequenceId) {
-                data.sequenceId = randomstring.generate(16);
-            }
-            data.timestamp = Date.now();
-            var strData = JSON.stringify(data);
-            this.redis.publish("event#client", strData);
+    var path = data.path;
+    var pushId = data.pushId;
+    if (path && pushId) {
+        if (!data.sequenceId) {
+            data.sequenceId = randomstring.generate(16);
         }
+        data.timestamp = Date.now();
+        var strData = JSON.stringify(data);
+        this.redis.publish("event#client", strData);
     }
 };
 
 PacketService.prototype.publishDisconnect = function (socket) {
-    if (this.redis) {
-        var self = this;
-        this.redis.get("pushIdSocketId#" + socket.pushId, function (err, lastSocketId) {
-            // reply is null when the key is missing
-            logger.debug("pushIdSocketId redis %s %s %s", socket.id, lastSocketId, socket.pushId);
-            if (lastSocketId == socket.id) {
-                logger.debug("publishDisconnect current socket disconnect %s", socket.id);
-                self.redis.del("pushIdSocketId#" + socket.pushId);
-                var data = {pushId: socket.pushId, path: "/socketDisconnect"};
-                if (socket.uid) {
-                    data.uid = socket.uid;
-                }
-                self.publishPacket(data);
+    var self = this;
+    this.redis.get("pushIdSocketId#" + socket.pushId, function (err, lastSocketId) {
+        // reply is null when the key is missing
+        logger.debug("pushIdSocketId redis %s %s %s", socket.id, lastSocketId, socket.pushId);
+        if (lastSocketId == socket.id) {
+            logger.debug("publishDisconnect current socket disconnect %s", socket.id);
+            self.redis.del("pushIdSocketId#" + socket.pushId);
+            var data = {pushId: socket.pushId, path: "/socketDisconnect"};
+            if (socket.uid) {
+                data.uid = socket.uid;
             }
-        });
-    }
+            self.publishPacket(data);
+        }
+    });
 };
 
 PacketService.prototype.publishConnect = function (socket) {
-    if (this.redis) {
-        var self = this;
-        this.redis.get("pushIdSocketId#" + socket.pushId, function (err, lastSocketId) {
-            // reply is null when the key is missing
-            if (lastSocketId) {
-                logger.debug("reconnect do not publish", lastSocketId);
-            } else {
-                logger.debug("first connect publish", lastSocketId);
-                var data = {pushId: socket.pushId, path: "/socketConnect"};
-                if (socket.uid) {
-                    data.uid = socket.uid;
-                }
-                self.publishPacket(data);
+    var self = this;
+    this.redis.get("pushIdSocketId#" + socket.pushId, function (err, lastSocketId) {
+        // reply is null when the key is missing
+        if (lastSocketId) {
+            logger.debug("reconnect do not publish", lastSocketId);
+        } else {
+            logger.debug("first connect publish", lastSocketId);
+            var data = {pushId: socket.pushId, path: "/socketConnect"};
+            if (socket.uid) {
+                data.uid = socket.uid;
             }
-            self.redis.set("pushIdSocketId#" + socket.pushId, socket.id);
-            self.redis.expire("pushIdSocketId#" + socket.pushId, 3600 * 24 * 7);
-        });
-    }
+            self.publishPacket(data);
+        }
+        self.redis.set("pushIdSocketId#" + socket.pushId, socket.id);
+        self.redis.expire("pushIdSocketId#" + socket.pushId, 3600 * 24 * 7);
+    });
 };
