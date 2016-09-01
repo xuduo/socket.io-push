fc5b3fd40312b9d9cb8f189331624c0ac74aa30d
xuduo <xuudoo@gmail.com>
2016-09-01 10:37:32 +0800

    change default log config

--- push-server/config-log.js
+++ push-server/config-log.js
@@ -3,26 +3,13 @@ let config = {};
 config.level = "debug";
 
 //可选 简单日志文件配置,配合formatter和timestamp可以接入ELK
-config.filename = 'log/history.log';
+//config.filename = 'log/history.log';
 
 //可选 轮转日志文件配置
 config.rotate_dir = 'log';
 
-config.formatter = function (args) {
-    let jsonObj = {};
-    jsonObj.id = args.meta.id;
-    jsonObj.timestamp = Date.now() / 1000 | 0;
-    jsonObj.service = "me_push";  //不同项目由service区分,一般修改这里就可以, 只能用数字小写字母和下划线
-    jsonObj.pid = process.pid;
-    jsonObj.ip = "##CTL_IP##";    //电信IP,对于没有电信IP的修改成其他
-    jsonObj.logtype = "event";
-    jsonObj.level = args.level;
-    jsonObj.module = args.meta.module;
-    jsonObj.msg = args.message;
-    return JSON.stringify(jsonObj);
-};
 
 //是否输出到console, 默认不输出
-config.console = false;
+config.console = true;
 
 module.exports = config;
