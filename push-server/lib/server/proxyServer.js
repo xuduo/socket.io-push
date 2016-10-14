2a24dcecb0a1ff8fd6c36e4f7ca886d0c0437b48
xuduo <xuudoo@gmail.com>
2016-10-14 10:48:48 +0800

    reachrate check topic noti

--- push-server/lib/server/proxyServer.js
+++ push-server/lib/server/proxyServer.js
@@ -34,19 +34,22 @@ function ProxyServer(io, stats, packetService, notificationService, uidStore, tt
         socket.on('pushId', function (data) {
             if (data.id && data.id.length >= 10) {
                 logger.debug("on pushId %j", data);
-                if (data.platform) {
-                    socket.platform = data.platform.toLowerCase();
-                    if(socket.platform == 'android'){
-                        stats.userLogin(data.id, Date.now());
-                    }
-                }
-                stats.addPlatformSession(socket.platform);
+
                 const topics = data.topics;
                 if (topics && topics.length > 0) {
                     topics.forEach(function (topic) {
                         socket.join(topic);
                     });
                 }
+
+                if (data.platform) {
+                    socket.platform = data.platform.toLowerCase();
+                    if(socket.platform == 'android' && topics && topics.contains("noti")){
+                        stats.userLogin(data.id, Date.now());
+                    }
+                }
+                stats.addPlatformSession(socket.platform);
+
                 socket.join(data.id, function (err) {
                     if (err) {
                         logger.error("join pushId room fail %s", err);
